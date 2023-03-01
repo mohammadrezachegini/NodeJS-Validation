2670789ca86db34394824a0e38ad60a681106899
mohammadrezachegini <mohammadreza.chegini01@gmail.com>
2023-02-28 22:14:33 -0800

    add validator for id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {NotFoundError, ErrorHandler} = require("./util/errorHandler");
 const {validationResult} = require("express-validator")
 const {loginValidator, registerValidator} = require("./validators/auth.validator");
 const {checkValidation} = require("./middlewares/validator");
+const {idValidator} = require("./validators/blog.validator");
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -14,6 +15,9 @@ app.post("/register",registerValidator(),checkValidation, (req,res,next) => {
     res.send(req.body);
 })
 
+app.get("blogs/:id", idValidator, checkValidation, (req,res) => {
+    res.send(req.params)
+})
 
 app.use(NotFoundError)
 app.use(ErrorHandler)
