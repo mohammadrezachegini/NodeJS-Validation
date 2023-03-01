3929e546f34505722008224c5d52a056caf3c52c
mohammadrezachegini <mohammadreza.chegini01@gmail.com>
2023-02-28 21:17:55 -0800

    initial app.js structure

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.post("/body", (req,res,next) => {
+    console.log(req.body)
+    res.send(req.body)
+})
+app.listen(3000, () => {
+    console.log("server is runing")
+})
